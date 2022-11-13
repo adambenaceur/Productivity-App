5b48dbebab048cdfaab80e0255d6f906647fb3d5
adambenaceur <benaceur.adam@gmail.com>
2022-11-13 14:38:46 -0700

    header for the productivity app (seperate from the home header)

---
+++ components/HeaderApp.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import Image from 'next/image';
+
+
+
+const HeaderApp = () => {
+  const router = useRouter()
+
+  const list = [
+    {
+      id: "/Dashboard",
+      title: 'Dashboard',
+    },
+    {
+      id: "/Habit-Tracker",
+      title: 'Habit Tracker',
+    },
+    {
+      id: "/Calander",
+      title: 'Calander',
+    },
+    {
+      id: "/To-do",
+      title: 'to-do list',
+    },
+    {
+        id: "/notes",
+        title: 'notes',
+      },
+    {
+        id: "/login",
+        title: 'log in!',
+      },   
+  ];
+
+
+  return (
+    <div className='header'>
+        <div className='nav-links'>
+            <ul className='nav-bar'>
+                {list.map((item) => (
+                <li key={item.id}>
+                    <Link href={item.id}>
+                        <a className={router.pathname === item.id ? "active" : ""}>{item.title}</a>
+                    </Link>
+                </li>
+                ))}
+            </ul>
+        </div>
+      <Link href='/'><button className='button'> Pause <Image src='/pause.svg' alt='' width='30px' height='30px'/> </button></Link> 
+    </div>
+  )
+}
+
+export default HeaderApp
