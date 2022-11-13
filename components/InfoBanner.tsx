dd1819d17eaf16f985d6fc6bc78fc692a79fa9e7
adambenaceur <benaceur.adam@gmail.com>
2022-11-13 14:39:59 -0700

    created info banner about tech stack for the productivity app

---
+++ components/InfoBanner.tsx
@@ -0,0 +1,24 @@
+const InfoBanner = () => {
+    return (
+        <div className='hero'>
+            <div className='hero-img' >
+                <img src='/Old-Computer-Task-Manager.png' alt='' />
+            </div>
+
+            <div className='hero-text'>
+                <h1>Tech Stacks</h1>
+                <h4> 
+                    <img src="/next-js.svg"  width={'50px'} height={'50px'} />
+                    <img src="/react-js.svg" width={'50px'} height={'50px'} /> 
+                    <img src="/vercel.svg" width={'100px'} height={'50px'} />
+                    <img src="/firebase.svg" width={'100px'} height={'50px'} /> 
+                    <img src="/typescript.svg" width={'50px'} height={'50px'} /> 
+                </h4>
+            </div>
+    
+        
+    </div>
+    )
+}
+
+export default InfoBanner
