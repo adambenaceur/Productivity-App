ecc834e4a56ddd6e6b0b5301fd7c221447435a12
adambenaceur <benaceur.adam@gmail.com>
2022-11-13 16:10:32 -0700

    changed hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
 
     <div className='hero-text'>
       <h1>Productivity Just A Click Away</h1>
-      <h4>The mind is a very powerful thing we possess and to take care of our mind is an utmost prioity. Productivity can take care for the mind to the next level.</h4>
+      <h4>The mind is a very powerful thing we possess and to take care of our mind is an utmost prioity. Let task manager take care for your mind to the next level.</h4>
 
   </div>
 
