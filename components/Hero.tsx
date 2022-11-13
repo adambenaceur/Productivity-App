8a990b40c0289e302a9c3bf43dc248fcf3dba395
adambenaceur <benaceur.adam@gmail.com>
2022-11-13 14:42:32 -0700

    created Hero section for productivity-app

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,9 +5,9 @@ const Hero = () => {
     <div className='hero'>
 
     <div className='hero-text'>
-      <h1>Astrano Self Training</h1>
-      <h4>The mind is a very powerful thing we possess and to take care of our mind is an utmost prioity. <br/> With Astrano, we take caring for the mind to the next level.  </h4>
-      <button>Begin Your Journey</button>
+      <h1>Productivity Just A Click Away</h1>
+      <h4>The mind is a very powerful thing we possess and to take care of our mind is an utmost prioity. Productivity can take care for the mind to the next level.</h4>
+
   </div>
 
     <div className='hero-img' >
